95ae5c6ee369e4620414e50b76f326cb84618497
Igor Sokolov <igorsok@2379591026.vpn.umich.net>
2018-10-21 15:30:25 -0400

    src/ModSize_orig.f90

--- src/ModSize_orig.f90
+++ src/ModSize_orig.f90
@@ -7,28 +7,14 @@ module SP_ModSize
 
   private ! except
   public:: &
-       nDim, nMomentum, nPitchAngle, IsPitchAngleAveraged, &
-       Particle_,  OriginLon_, OriginLat_, &
-       nLon, nLat, nNode, nParticleMax
+       nDim, nMomentum, nPitchAngle, IsPitchAngleAveraged, & 
+       nParticleMax
 
   ! Dimensionality
   integer, parameter:: nDim = 3
-
-  ! Indices of internal cooridnates
-  integer, parameter:: &
-       Particle_  = 1, & ! Index along  field line
-       OriginLon_ = 2, & ! Longitude of field line origin
-       OriginLat_ = 3    ! Latitude  of field line origin
-
       
-
   ! Max possible index of a particle on a line set by Config.pl
   integer, parameter:: nParticleMax = 1000
-
-  ! angular grid at origin surface
-  integer, parameter:: nLon  = 4
-  integer, parameter:: nLat  = 4
-  integer, parameter:: nNode = nLon*nLat
 
   ! number of points along the phase coords (see ModAdvance);
   integer, parameter:: nMomentum   = 100
