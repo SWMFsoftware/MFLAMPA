2729b981be7c91c6c5e45d8eb104d95b10a8a128
Igor V. Sokolov <igorsok@umich.edu>
2024-02-11 01:01:56 -0500

    Improve comments, remove redundant options

--- src/ModAdvanceAdvection.f90
+++ src/ModAdvanceAdvection.f90
@@ -5,29 +5,6 @@ module SP_ModAdvanceAdvection
 
   use ModUtilities, ONLY: CON_stop
   use SP_ModDistribution, ONLY: nP, Momentum_I, Distribution_IIB, dLogP
-  ! Solves advection in the momentum space (=first order Fermi acceleration)
-  ! First way - solve advection over log P coordinate.
-  ! In space physics applications one often needs to solve the
-  ! "log-advection" equations of the kind of:
-  !         f_t+A*f_lnp=0             (NC)
-  ! or
-  !         f_t+A*(pf)_p=0 .          (C)
-  ! The CONSERVATION LAW Eq.(C) can be also written as
-  !         f_t+A*f_lnp+ A*f=0,
-  ! which is the total of the NONCONSERVATIVE Eq.(NC) plus Af
-  ! Both Eqs.(NC,C) propagate the initial values of f along the lines
-  !         p=p_0*exp(At),
-  ! which results in a uniform expansion (A>0) or shrinking (A<0) of
-  ! the distribution, keeping its shape unchanged. For Eq.(C) the solution
-  ! also scales accordingly, keeping unchanged the integral \int{f dp}
-  !
-  ! EXAMPLES: Non-conservative formulation: first-order Fermi acceleration,
-  !           Alfven-wave turbulence evolution in the solar wind
-  !           Conservative formulation: Kolmogorov-type cascade,
-  !           wave-particle interactions
-  !
-  ! In all these applications, an acceleration rate, A, does not depend on the
-  ! phase coordinate, p
   implicit none
   ! Revision history
   ! Prototype: Sokolov&Roussev, FLAMPA code, 2004
@@ -98,7 +75,7 @@ contains
              RETURN
           end if
           call advance_log_advection(FermiFirst_I(iVertex), &
-               1, 1, Distribution_IIB(0:nP+1,iVertex,iLine), .false.)
+               1, 1, Distribution_IIB(0:nP+1,iVertex,iLine))
        end do
        ! compute diffusion along the field line
        ! set the left boundary condition (for diffusion)
@@ -109,29 +86,25 @@ contains
     end do STEP
   end subroutine advect_via_log
   !============================================================================
-  subroutine advance_log_advection(CFLIn, nGCLeft, nGCRight,        &
-       FInOut_I, IsConservative, DeltaLnP)
+  subroutine advance_log_advection(CflIn, nGCLeft, nGCRight, FInOut_I)
     ! The procedure integrates the log-advection equation, in
-    ! the conservative or non-conservative formulation, at a logarithmic grid,
+    ! non-conservative formulation, at a logarithmic grid,
     ! using a single-stage second order scheme
 
-    real,   intent(in):: CFLIn        ! Time step * acceleration rate/(Dlnp)
+    real,   intent(in):: CflIn        ! Time step * acceleration rate/(Dlnp)
     integer,intent(in):: nGCLeft      ! The solution in the ghost cells is not
     integer,intent(in):: nGCRight     ! advanced in time but used as the BCs
     real,intent(inout):: FInOut_I(1-nGCLeft:nP+nGCRight)  ! Solution
     ! sol. index 0 is to set boundary condition at the injection energy
-    logical,intent(in):: IsConservative  ! Solve (C) if .true. (NC) otherwise
-    real,optional,intent(in)::DeltaLnP   ! Used only for NonConservative=.true.!
     ! Subcycling to achieve the condition CFL<1, if nStep>1
     integer :: nStep
     ! Loop variables
     integer :: iStep
     ! Extended version of the sulution array to implement BCc
     real    :: F_I(1 - max(nGCLeft,2):nP+max(nGCRight,2))
-    real    :: FSemiintUp_I(0:nP+1), FSemiintDown_I(0:nP+1)
-    real    :: CFL, HalfADtIfNeeded
+    real    :: FSemiintUp_I(0:nP+1), FSemiintDown_I(0:nP+1), Cfl
     !-------------------------NonConservative---------------------------
-    ! This is a one-stage second-order scheme for the advection equation:
+    ! This is a single-stage second-order scheme for the advection equation:
     !             f_t+A f_{ln p}=0,
     !
     ! Herewith CFL = A*Delta t/Delta(ln p):
@@ -140,43 +113,15 @@ contains
     ! f^n_(i-1/2)=f^n_(i-1)+cHalf*(cOne-CFL)*df_lim^n_(i-1)
     ! For CFL<0:
     ! f^n_(i-1/2)=f^n_(i  )-cHalf*(cOne+CFL)*df_lim^n_(i  )
-    !-------------------------   Conservative---------------------------
-    ! This is a one-stage second-order scheme for the advection equation:
-    !             f_t+A (f p}_p=0,
-    ! We now need to use both
-    ! A*Delta t = CFLIn*Delta (LnP)
-    ! and
-    ! CFL = A*Delta t/(2 tanh(Delta (ln p)/2)
-    ! f^(n+1)_i = CFL*(f^n_(i-1/2)-f^n_(i+1/2))-
-    !        (1/2)(f^n_(i-1/2)+f^n_(i+1/2))A*Delta t + f^n_i,
-    ! where
-    ! For CFL>0:
-    ! f^n_(i-1/2)=f^n_(i-1)*(1-(1/2)*A*Delta t)+cHalf*(cOne-CFL)*df_lim^n_(i-1)
-    ! For CFL<0:
-    ! f^n_(i-1/2)=f^n_(i  )*(1-(1/2)*A*Delta t)-cHalf*(cOne+CFL)*df_lim^n_(i  )
-
     !--------------------------------------------------------------------------
-    if(IsConservative)then
-       HalfADtIfNeeded = 0.50*CFLIn*DeltaLnP
-       CFL = HalfADtIfneeded/tanh(0.50 * DeltaLnP)
-       HalfADtIfNeeded = -HalfADtIfNeeded
-    else
-       HalfADtIfNeeded = 0.0; CFL = CFLIn
-    end if
 
-    nStep = 1 + int(abs(CFL)); CFL = CFL/real(nStep)
-    HalfADtIfNeeded=HalfADtIfNeeded/real(nStep)
-    F_I(1-nGCLeft:nP+nGCRight) = FInOut_I(1 - nGCLeft:nP+nGCRight)
+    nStep = 1 + int(abs(CflIn)); Cfl = CflIn/real(nStep)
+    F_I(1-nGCLeft:nP+nGCRight) = FInOut_I(1-nGCLeft:nP+nGCRight)
 
     ! Check for positivity
-    if(any(F_I(1-nGCLeft:nP+nGCRight)<=0.0))then
-       write(*,*)'Before advection F_I <=0'
-       write(*,*)IsConservative
-       write(*,*)F_I
-       stop
-    end if
-
-    ! One stage second order upwind scheme
+    if(any(F_I(1-nGCLeft:nP+nGCRight)<=0.0))call CON_stop(&
+         'Negative distribution function before log advection')
+    ! Single stage second order upwind scheme
 
     if (CFL>0.0) then
        do iStep = 1, nStep
@@ -186,14 +131,13 @@ contains
           if(nGCRight<2)F_I(nP+1-nGCRight:nP+2     ) = F_I(nP+nGCRight)
 
           ! f_(i+1/2):
-          FSemiintUp_I(0:nP) = F_I(0:nP)*(1.0 + HalfADtIfNeeded)&
+          FSemiintUp_I(0:nP) = F_I(0:nP)&
                + 0.5*(1.0 - CFL)*df_lim_array(0, nP)
           ! f_(i-1/2):
           FSemiintDown_I(1:nP) = FSemiintUp_I(0:nP-1)
 
           ! Update the solution from f^(n) to f^(n+1):
-          F_I(1:nP) = F_I(1:nP)+CFL*(FSemiintDown_I(1:nP)-FSemiintUp_I(1:nP))+&
-               HalfADtIfNeeded*(FSemiintDown_I(1:nP)+FSemiintUp_I(1:nP))
+          F_I(1:nP) = F_I(1:nP)+Cfl*(FSemiintDown_I(1:nP)-FSemiintUp_I(1:nP))
        end do
     else
        do iStep = 1, nStep
@@ -203,23 +147,19 @@ contains
           if(nGCRight<2)F_I(nP+1-nGCRight:nP+2     ) = F_I(nP+nGCRight)
 
           ! f_(i-1/2):
-          FSemiintDown_I(1:nP+1) = F_I(1:nP+1)*(1.0 + HalfADtIfNeeded)&
+          FSemiintDown_I(1:nP+1) = F_I(1:nP+1)&
                - 0.5*(1.0 + CFL)*df_lim_array(1, nP+1)
           ! f_(i+1/2):
           FSemiintUp_I(1:nP) = FSemiintDown_I(2:nP+1)
 
           ! Update the solution from f^(n) to f^(n+1):
-          F_I(1:nP) = F_I(1:nP)+CFL*(FSemiintDown_I(1:nP)-FSemiintUp_I(1:nP))+&
-               HalfADtIfNeeded*(FSemiintDown_I(1:nP)+FSemiintUp_I(1:nP))
+          F_I(1:nP) = F_I(1:nP)+Cfl*(FSemiintDown_I(1:nP)-FSemiintUp_I(1:nP))
        end do
     end if
 
     FInOut_I(1:nP) = F_I(1:nP)
-    if(any(FInOut_I(1-nGCLeft:nP+nGCRight)<=0.0))then
-       write(*,*)'After advection F_I <=0, for CFLFermi= ',CFL
-       write(*,*)F_I
-       call CON_stop('Negative distribution function')
-    end if
+    if(any(FInOut_I(1-nGCLeft:nP+nGCRight)<=0.0))call CON_stop(&
+         'Negative distribution function after log advection')
   contains
     !==========================================================================
     function df_lim_array(iLeft, iRight) result(df_lim_arr)
