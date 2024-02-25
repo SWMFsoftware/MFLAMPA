81b75f41c3953ea501fb8717dea603b9259638c0
lwh1106 <whliu@umich.edu>
2024-02-25 02:59:39 -0500

    minor change to script for subsequent comparison

--- Scripts/mflampa_1d_fluxcompare.py
+++ Scripts/mflampa_1d_fluxcompare.py
@@ -11,14 +11,14 @@ cols=['LagrID', 'X', 'Y', 'Z', 'Rho', 'T', 'Ux', 'Uy', 'Uz', 'Bx', 'By', 'Bz',
        'Wave1', 'Wave2', 'flux_total', 'flux_00005', 'flux_00010',
        'flux_00030', 'flux_00050', 'flux_00060', 'flux_00100', 'eflux']
 mu0=1.25663706E-6
-rootdir='/Users/whliu/SWMF/SP/MFLAMPA/Doc/'
+rootdir='/Users/whliu/test3/SWMF/SP/MFLAMPA/Doc/'
 # rootdir='../Doc/SP-New/IO2/MH_data_001_001_e20120123_040000_n000000.out'
 
 nlon=4
 nlat=4
 ntime=1
 
-def plot_1d_flux_1line_1time(filename, filenameOld, ax):
+def plot_1d_flux_1line_1time(fileList, ax):
     
     def read_outfile(filename):
         data=pd.read_csv(filename,skiprows=16,sep='\s+',names=cols)
@@ -31,24 +31,18 @@ def plot_1d_flux_1line_1time(filename, filenameOld, ax):
         
         return data
 
-    dataNew = read_outfile(filename)
-    dataOld = read_outfile(filenameOld)
-
-    ax[0,0].plot(dataNew['r'],dataNew['rho']*dataNew['r']**2.,color=colors[ifile])
-    ax[1,0].plot(dataNew['r'],dataNew['u'],color=colors[ifile])
-    ax[2,0].plot(dataNew['r'],dataNew['db2']/dataNew['b']**2,color=colors[ifile])
-
-    ax[0,1].plot(dataNew['r'],dataNew['flux_00005'],label="Advect Via \nPossion Bracket Alg.",color=colors[ifile])
-    ax[1,1].plot(dataNew['r'],dataNew['flux_00010'],color=colors[ifile])
-    ax[2,1].plot(dataNew['r'],dataNew['flux_00100'],color=colors[ifile])
+    dataTest = [read_outfile(fileList[iFile]) for iFile in range(len(fileList))]
+    dataLegend = ['Test: Non-\nConservative Advection', 'Test: Conservative \nPoisson Advection', 'Test: Poisson and GCR \nSpectrum at 1 AU']
     
-    ax[0,0].plot(dataOld['r'],dataOld['rho']*dataOld['r']**2., ls='-.', color=colors[-ifile-1])
-    ax[1,0].plot(dataOld['r'],dataOld['u'], ls='-.', color=colors[-ifile-1])
-    ax[2,0].plot(dataOld['r'],dataOld['db2']/dataOld['b']**2, ls='-.', color=colors[-ifile-1])
+    for iFile in range(3):
+        print(iFile, dataTest[iFile]['flux_00005'][0:10])
+        ax[0,0].plot(dataTest[iFile]['r'],dataTest[iFile]['rho']*dataTest[iFile]['r']**2., ls='-.', color=colors[-iFile-2])
+        ax[1,0].plot(dataTest[iFile]['r'],dataTest[iFile]['u'], ls='-.', color=colors[-iFile-2])
+        ax[2,0].plot(dataTest[iFile]['r'],dataTest[iFile]['db2']/dataTest[iFile]['b']**2, ls='-.', color=colors[-iFile-2])
 
-    ax[0,1].plot(dataOld['r'],dataOld['flux_00005'],label="Advect Via \nNon-Conservative Alg.",ls='-.', color=colors[-ifile-1])
-    ax[1,1].plot(dataOld['r'],dataOld['flux_00010'],ls='-.', color=colors[-ifile-1])
-    ax[2,1].plot(dataOld['r'],dataOld['flux_00100'],ls='-.', color=colors[-ifile-1])
+        ax[0,1].plot(dataTest[iFile]['r'],dataTest[iFile]['flux_00005'],label=dataLegend[iFile],ls='-.', color=colors[-iFile-2])
+        ax[1,1].plot(dataTest[iFile]['r'],dataTest[iFile]['flux_00010'],ls='-.', color=colors[-iFile-2])
+        ax[2,1].plot(dataTest[iFile]['r'],dataTest[iFile]['flux_00100'],ls='-.', color=colors[-iFile-2])
 
     ax[0,0].set_ylabel('$\\rho r^2$')
     ax[1,0].set_ylabel('U [km/s]')
@@ -64,9 +58,9 @@ def plot_1d_flux_1line_1time(filename, filenameOld, ax):
     ax[1,0].set_ylim(0,800)
     ax[2,0].set_ylim(1E-4,1E+2)
     
-    ax[0,1].set_ylim(9E-3,1.2E-2)
-    ax[1,1].set_ylim(9E-3,1.2E-2)
-    ax[2,1].set_ylim(9E-3,1.2E-2)
+    ax[0,1].set_ylim(5E-3,5E-1)
+    ax[1,1].set_ylim(5E-3,5E-1)
+    ax[2,1].set_ylim(5E-3,5E-1)
     
     ax[0,1].legend(prop={"size": 9})
     
@@ -82,19 +76,20 @@ def plot_1d_flux_1line_1time(filename, filenameOld, ax):
 for ilon in range(1,nlon+1):
     for ilat in range(1,nlat+1):
         
-        files_new = sorted(glob.glob(rootdir+'SP-New/IO2/MH_data_'+str(ilon).zfill(3)+'_'+str(ilat).zfill(3)+'*.out'))
-        files_old = sorted(glob.glob(rootdir+'SP-Old/IO2/MH_data_'+str(ilon).zfill(3)+'_'+str(ilat).zfill(3)+'*.out'))
-        colors = [matplotlib.cm.rainbow(x) for x in np.linspace(0, 1, len(files_new))]
+        files_mflampa = sorted(glob.glob(rootdir+'run_mflampa/IO2/MH_data_'+str(ilon).zfill(3)+'_'+str(ilat).zfill(3)+'*.out'))
+        files_poisson = sorted(glob.glob(rootdir+'run_poisson/IO2/MH_data_'+str(ilon).zfill(3)+'_'+str(ilat).zfill(3)+'*.out'))
+        files_poissoncrphi = sorted(glob.glob(rootdir+'run_poissoncrphi/IO2/MH_data_'+str(ilon).zfill(3)+'_'+str(ilat).zfill(3)+'*.out'))
+        colors = [matplotlib.cm.rainbow(x) for x in np.linspace(0, 1, len(files_mflampa))]
         
         fig, ax = plt.subplots(3,2,figsize=(6,6))
-        for ifile, filename in enumerate(files_new):
+        for ifile, filename1 in enumerate(files_mflampa):
             if ifile == ntime:
-                filenameOld = files_old[ifile]
-                timestr = filename[-27:-12]
-                plot_1d_flux_1line_1time(filename, filenameOld, ax)
+                timestr = filename1[-27:-12]
+                filename2 = files_poisson[ifile]
+                filename3 = files_poissoncrphi[ifile]
+                plot_1d_flux_1line_1time([filename1, filename2, filename3], ax)
                 fig.suptitle('ilon_ilat = '+str(ilon).zfill(3)+'_'+str(ilat).zfill(3)+' at %s'%(timestr))
         plt.subplots_adjust(left=0.13, bottom=0.08, right=0.98, top=0.93, wspace=0.6, hspace=0.2)
-        fig.savefig(rootdir+'Fig/Compare/mhd_sep_'+str(ilon).zfill(3)+'_'+str(ilat).zfill(3)+'_%.2d.pdf'%(ntime),dpi=450)
-        plt.close(fig) 
-        plt.show()
+        fig.savefig(rootdir+'Fig/Compare/mhd_sep_'+str(ilon).zfill(3)+'_'+str(ilat).zfill(3)+'_%.2d_.pdf'%(ntime),dpi=450)
+        plt.show(); plt.close(fig)
         exit()
